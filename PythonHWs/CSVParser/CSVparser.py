0feda6cdd19e80cac775d3fe766b47b846ca88d8
Аникеев Георгий Леонидович <anikeee@mail.ru>
2021-03-16 18:22:48 +0300

    v2

--- PythonHWs/CSVParser/CSVparser.py
+++ PythonHWs/CSVParser/CSVparser.py
@@ -1,20 +1,33 @@
 import sys
 import csv
 
-def getDeps(file):
+def getDeps(file: str) -> list:
+    """
+    На входе получает путь до CSV-файла 
+    Формирует список отделов из файла file 
+    Возвращает лист со списком отделов
+    """
     op = open(file, 'r')
     reader = csv.reader(op, delimiter=';')
     departments = {}
     for line in reader:
         departments[line[2]] = ''
-    return departments
+    op.close()
 
-def makeAReport(file):
-    report = getDeps(file)
+    return list(departments.keys())
+
+def makeAReport(file: str) -> dict:
+    """
+    На входе получает путь до CSV-файла
+    Формирует отчёт на основе списка отделов, полученных 
+    из функции getDeps
+    Возвращает словарь со сводным отчётом по отделам
+    """
+    deps = getDeps(file)
     op = open(file, 'r')
     reader = csv.reader(op, delimiter=';')
-
-    for keys in report.keys():
+    report = {}
+    for keys in deps:
         report[keys] = dict([
             ('num', 0), 
             ('min', 100_000_000),
@@ -31,15 +44,19 @@ def makeAReport(file):
             ('max', l2['max'] if l2['max'] > zp else zp),
             ('avg', l2['avg'] + [zp]),
             ])
+    op.close()
     
     for keys in report.keys():
-        report[keys]['avg'] = sum(report[keys]['avg']) / len(report[keys]['avg'])
+        avg = report[keys]['avg']
+        report[keys]['avg'] = sum(avg) / len(avg)
     return report
 
-def printAReport(file):
-    makeAReport(file)
-
-def saveAReport(file):
+def saveAReport(file: str) -> str:
+    """
+    На входе получает путь до CSV-файла
+    Сохраняет сводный отчёт по отделам в файл с названием "result.csv"
+    Возвращает "success", если всё удачно
+    """
     report = makeAReport(file)
     output = open('result.csv', 'w')
     writer = csv.writer(output, delimiter=';')
@@ -54,21 +71,33 @@ def saveAReport(file):
             report[keys]['num'],
             report[keys]['min'],
             report[keys]['max'],
-            report[keys]['avg'],
+            report[keys]['avg']
             ])
     output.close()
     return 'success'
 
 
-def process():
-    file = sys.argv[1]
+def process() -> None:
+    """
+    Запускает программу, сохраняет первый параметр как путь до исходного
+    CSV-файла, просит пользователя сделать выбор
+    """
+    file = None
+    try:
+        file = open(sys.argv[1], 'r')
+    except FileNotFoundError:
+        print('Нет такого файла')
+        exit(1)
+    kek = None
+    kek = open('1', 'w')
+
     print('Что делать?\n'
           '1 - вывести все отделы\n'
           '2 - вывести сводный отчёт по отделам\n'
-          '3 - вывести сводный отчёт по отделам в csv-файл\n')
+          '3 - вывести сводный отчёт по отделам в CSV-файл\n')
     options = {
         '1': getDeps,
-        '2': printAReport,
+        '2': makeAReport,
         '3': saveAReport,
         }
     result = options[input()](file)
@@ -76,6 +105,8 @@ def process():
 
 if len (sys.argv) == 1:
     print('дайте файл')
+    print('Нажмите Enter для падения')
+    input()
     sys.exit(1)
 
 process()
